438c620b71a659369535c577b5594a6f198a1fa5
joseph <giuseppemanna1998@icloud.com>
2025-07-06 11:41:22 +0200

    ex6

---
+++ src/ex6.java
@@ -0,0 +1,11 @@
+public class ex6 {
+    public static void main(String[] args) {
+        int [] a = {1, 2, 3, 6, 5};
+
+        for (int i = a.length - 1; i > 0; i--) {
+            int array = a[i];
+            System.out.println(array);
+
+        }
+    }
+}
