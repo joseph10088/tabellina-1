af93d11f67471c00efc325eeecd371882cfadbcf
joseph <giuseppemanna1998@icloud.com>
2025-07-06 13:31:22 +0200

    ex10

---
+++ src/ex10.java
@@ -0,0 +1,15 @@
+public class ex10 {
+    public static void main(String[] args) {
+        int column = 6;
+        int line = 4;
+
+        for (int i = 1; i <= line; i++) {
+
+            for (int j = 1; j <= column; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+            
+        }
+    }
+}
